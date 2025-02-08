3bb4cebe9b215b35f15327d35351b07554d6ec47
dhanshripatil720 <dhanshripatil720@gmail.com>
2025-02-08 23:44:14 +0530

    Create variables.tf

---
+++ threetier/variables.tf
@@ -0,0 +1,18 @@
+variable "my_region" {
+   type = string
+}
+variable "access_key" {
+   type = string
+}
+variable "secret_key" {
+   type =string
+}
+variable "my_ami" {
+   type = string
+}
+variable "vpc_id" {
+  type = string
+}
+variable "ins_type" {
+  type = string
+}
